fd145554d636b96f7a1d4ac037bda34c9b7c11c5
Geir H. Bolstad <geir.bolstad@nina.no>
2019-05-24 10:49:35 +0200

    Included A and B in the plot.rate_gls() and changed output to number of significant digits

--- R/round_and_format.R
+++ R/round_and_format.R
@@ -3,13 +3,15 @@
 #' \code{round_and_format} rounds and formats data
 #'
 #' @param x data in numeric format
-#' @param digits Number of decimals
+#' @param digits Number of decimal places
+#' @param sign_digits Number of significant digits (if given this overrides \code{digits})
 #' @param scientfic Logical specifying whether encoding in scientific notation
 #' @return rounded and formatted values
 #' @author Geir H. Bolstad
 #'
 #' @export
 
-round_and_format <- function(x, digits = 2, scientific = FALSE, trim = TRUE){
-  format(round(x, digits = digits), digits = digits, nsmall = digits, scientific = scientific, trim = trim)
+round_and_format <- function(x, digits = 2, sign_digits = NULL, scientific = FALSE, trim = TRUE){
+  if(is.null(sign_digits)) format(round(x, digits = digits), digits = digits, nsmall = digits, scientific = scientific, trim = trim)
+  else format(signif(x, digits = sign_digits), digits = sign_digits, nsmall = sign_digits, scientific = scientific, trim = trim)
 }
