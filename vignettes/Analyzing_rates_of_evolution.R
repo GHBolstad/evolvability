59e021293ca78cbe6a3fb12ecee7fe60aeb1abf5
Geir H. Bolstad <geir.bolstad@nina.no>
2020-04-23 15:38:21 +0200

    Updated vignettes

--- vignettes/Analyzing_rates_of_evolution.R
+++ vignettes/Analyzing_rates_of_evolution.R
@@ -2,51 +2,37 @@
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
-  eval = FALSE #NB! må kjøre modellane på nytt pga endring til A og B i staden for intercept og slope
+  eval = TRUE 
 )
 library(evolvability)
 
 ## -----------------------------------------------------------------------------
-#  tree <- ape::rtree(n = 10)
-#  tree <- ape::chronopl(tree, lambda = 1, age.min = 2)
+set.seed(102)
+tree <- ape::rtree(n = 50)
+tree <- ape::chronopl(tree, lambda = 1, age.min = 2)
 
-## ---- echo=FALSE--------------------------------------------------------------
-#  save(tree, file = "../data/tree.RData") # the model output is stored as it takes too long time to run within the vignette
-
-## ---- echo=FALSE, eval = TRUE-------------------------------------------------
-load("../data/tree.RData")
-
-## ---- eval = TRUE-------------------------------------------------------------
+## ---- eval=TRUE---------------------------------------------------------------
 ape::is.ultrametric(tree)
 
-## -----------------------------------------------------------------------------
-#  tree$edge.length <- tree$edge.length/diag(ape::vcv(tree))[1]
-
-## -----------------------------------------------------------------------------
-#  sim_data <- simulate_rate(tree, startv_x=0, sigma_x=0.25, a=1, b=1, model = "predictor_BM")
-
-## -----------------------------------------------------------------------------
-#  gls_mod <- rate_gls(x=sim_data$x, y=sim_data$y, species=sim_data$species, tree, model = "predictor_BM")
-
-## ---- echo=FALSE--------------------------------------------------------------
-#  save(gls_mod, file = "../data/mod_BM.RData") # the model output is stored as it takes too long time to run within the vignette
-
-## ---- echo=FALSE, eval = TRUE-------------------------------------------------
-load("../data/mod_BM.RData")
+## ---- eval=TRUE---------------------------------------------------------------
+tree$edge.length <- tree$edge.length/diag(ape::vcv(tree))[1]
 
 ## ---- eval = TRUE-------------------------------------------------------------
+plot(tree)
+
+## -----------------------------------------------------------------------------
+sim_data <- simulate_rate(tree, startv_x=0, sigma_x=0.25, a=2, b=1, model = "predictor_BM")
+head(sim_data)
+
+## -----------------------------------------------------------------------------
+gls_mod <- rate_gls(x=sim_data$x, y=sim_data$y, species=sim_data$species, tree, 
+                    model = "predictor_BM", silent = TRUE)
 gls_mod$param
 
 ## -----------------------------------------------------------------------------
-#  bootout <- boot.rate_gls(gls_mod, n = 10)
+bootout <- rate_gls_boot(gls_mod, n = 10, silent = TRUE)
 
-## ---- echo=FALSE--------------------------------------------------------------
-#  save(bootout, file = "../data/mod_BM_boot.RData") # the bootstrap output is stored as it takes too long time to run within the vignette
-
-## ---- echo=FALSE, eval = TRUE-------------------------------------------------
-load("../data/mod_BM_boot.RData")
-
-## ---- eval = TRUE-------------------------------------------------------------
+## -----------------------------------------------------------------------------
 bootout$summary
 
 ## ---- fig.height=5, fig.width=5, eval = TRUE----------------------------------
@@ -56,30 +42,17 @@ plot(gls_mod, scale = "VAR")
 plot(gls_mod) # with the default: scale == "SD"
 
 ## -----------------------------------------------------------------------------
-#  sim_data <- simulate_rate(tree, startv_x=1, sigma_x=2, a=2, b=0.1, model = "predictor_gBM")
+sim_data <- simulate_rate(tree, startv_x=1, sigma_x=1, a=1, b=0.1, model = "predictor_gBM")
+head(sim_data)
 
 ## -----------------------------------------------------------------------------
-#  gls_mod <- rate_gls(x=sim_data$x, y=sim_data$y, species=sim_data$species, tree, model = "predictor_gBM")
-
-## ---- echo=FALSE--------------------------------------------------------------
-#  save(gls_mod, file = "../data/mod_gBM.RData") # the model output is stored as it takes too long time to run within the vignette
-
-## ---- echo=FALSE, eval = TRUE-------------------------------------------------
-load("../data/mod_gBM.RData")
-
-## ---- eval = TRUE-------------------------------------------------------------
+gls_mod <- rate_gls(x=sim_data$x, y=sim_data$y, species=sim_data$species, tree, model = "predictor_gBM", silent = TRUE)
 gls_mod$param
 
 ## -----------------------------------------------------------------------------
-#  bootout <- boot.rate_gls(gls_mod, n = 10)
+bootout <- rate_gls_boot(gls_mod, n = 10, silent = TRUE) 
 
-## ---- echo=FALSE--------------------------------------------------------------
-#  save(bootout, file = "../data/mod_gBM_boot.RData") # the bootstrap output is stored as it takes too long time to run within the vignette
-
-## ---- echo=FALSE, eval = TRUE-------------------------------------------------
-load("../data/mod_gBM_boot.RData")
-
-## ---- eval = TRUE-------------------------------------------------------------
+## -----------------------------------------------------------------------------
 bootout$summary
 
 ## ---- fig.height=5, fig.width=5, eval = TRUE----------------------------------
@@ -89,28 +62,15 @@ plot(gls_mod, scale = "VAR")
 plot(gls_mod) # with the default scale == "SD"
 
 ## -----------------------------------------------------------------------------
-#  sim_data <- simulate_rate(tree, startv_x=0, sigma_x=1, a=1, b=1, sigma_y = 1, model = "recent_evol")
+sim_data <- simulate_rate(tree, startv_x=0, sigma_x=1, a=1, b=1, sigma_y = 1, model = "recent_evol")
+head(sim_data)
 
 ## -----------------------------------------------------------------------------
-#  gls_mod <- rate_gls(x=sim_data$x, y=sim_data$y, species=sim_data$species, tree, model = "recent_evol", useLFO = FALSE)
-
-## ---- echo=FALSE--------------------------------------------------------------
-#  save(gls_mod, file = "../data/mod_recentevol.RData") # the model output is stored as it takes too long time to run within the vignette
-
-## ---- echo=FALSE, eval = TRUE-------------------------------------------------
-load("../data/mod_recentevol.RData")
-
-## ---- eval = TRUE-------------------------------------------------------------
+gls_mod <- rate_gls(x=sim_data$x, y=sim_data$y, species=sim_data$species, tree, model = "recent_evol", useLFO = FALSE, silent = TRUE)
 gls_mod$param
 
 ## -----------------------------------------------------------------------------
-#  bootout <- boot.rate_gls(gls_mod, n = 10)
-
-## ---- echo=FALSE--------------------------------------------------------------
-#  save(bootout, file = "../data/mod_recentevol_boot.RData") # the bootstrap output is stored as it takes too long time to run within the vignette
-
-## ---- echo=FALSE, eval = TRUE-------------------------------------------------
-load("../data/mod_recentevol_boot.RData")
+bootout <- rate_gls_boot(gls_mod, n = 10, useLFO = FALSE, silent = TRUE) 
 
 ## ---- eval = TRUE-------------------------------------------------------------
 bootout$summary
--- vignettes/Phylogenetic_mixed_model.R
+++ vignettes/Phylogenetic_mixed_model.R
@@ -3,9 +3,12 @@ knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
+library(evolvability)
 
 ## -----------------------------------------------------------------------------
-n_species <- 100
+# Only a very small sample size is used 
+# in the interest of computational speed:
+n_species <- 20 
 tree <- ape::rtree(n = n_species)
 tree <- ape::chronopl(tree, lambda = 1)
 
@@ -16,12 +19,11 @@ A <- Matrix::Matrix(ape::vcv(tree), sparse = TRUE)
 colnames(A) <- rownames(A) <- paste("species", 1:n_species, sep = "_")
 
 ## -----------------------------------------------------------------------------
-y <- 5 + t(chol(A))%*%rnorm(n_species, 0, 2) + # BM process with mean 5 and s.d. 2
-     rnorm(n_species, 0, 1)                    # residual variation with s.d. 1
+y <- 5 + t(chol(A))%*%rnorm(n_species, 0, 2) + # BM process with mean = 5 and sd = 2
+     rnorm(n_species, 0, 1)                    # residual variation with sd = 1
 
 ## -----------------------------------------------------------------------------
-dt <- data.frame(species = colnames(A),
-                 y = as.vector(y))
+dt <- data.frame(species = colnames(A), y = as.vector(y))
 
 ## -----------------------------------------------------------------------------
 mod <- Almer(y ~ 1 + (1|species), data = dt, A = list(species = A))
@@ -35,45 +37,19 @@ mod_SE <- Almer_SE(y ~ 1 + (1|species), data = dt, SE = dt$SE, A = list(species 
 summary(mod_SE)
 
 ## -----------------------------------------------------------------------------
-sim_y <- Almer_sim(mod_SE, nsim = 3)
+sim_y <- Almer_sim(mod, nsim = 3)
 sim_y[1:3,]
 
-## ---- eval = FALSE------------------------------------------------------------
-#  dt$group <- c(rep("A", 50), rep("B", 50))
-#  mod <- Almer(y ~ group + (1|species), data = dt, A = list(species = A))
-#  #mod <- Almer(y ~ 1 + (1|species), data = dt, A = list(species = A))
-#  nsim = 3
-#  y <- Almer_sim(mod, nsim)
-#  dt <- mod@frame
-#  boot <- apply(y, 2, function(x){
-#    dt$hopefullynotavariablealready <- x
-#    update(mod, hopefullynotavariablealready ~ ., data = dt)
-#  })
-#  fixef_boot <- sapply(boot, lme4::fixef)
-#  if(is.vector(fixef_boot)){
-#    fixef_boot <- cbind(fixef_boot)
-#  }else fixef_boot <- t(fixef_boot)
-#  colnames(fixef_boot) <- names(lme4::fixef(boot[[1]]))
-#  
-#  VarCorr_list <- lapply(boot, function(x) as.data.frame(lme4::VarCorr(x)))
-#  vcov_boot <- t(sapply(VarCorr_list, function(x) x$vcov))
-#  colnames(vcov_boot) <- VarCorr_list[[1]]$grp
-#  
-#  list(
-#    fixef = cbind(mean = apply(fixef_boot, 2, mean),
-#        std.err. = apply(fixef_boot, 2, sd),
-#        "2.5% quantile" = apply(fixef_boot, 2, quantile, probs = 0.025),
-#        "97.5% quantile" = apply(fixef_boot, 2, quantile, probs = 0.975)
-#        ),
-#    vcov = cbind(mean = apply(vcov_boot, 2, mean),
-#        std.err. = apply(vcov_boot, 2, sd),
-#        "2.5% quantile" = apply(vcov_boot, 2, quantile, probs = 0.025),
-#        "97.5% quantile" = apply(vcov_boot, 2, quantile, probs = 0.975)
-#        ),
-#    models <- boot)
-#  
-#  
+## -----------------------------------------------------------------------------
+# The number of bootstrap simulations is kept very low in the interest 
+# of computational speed. Often 1000 is used in real analyses.
+Almer_boot_obj <- Almer_boot(mod, nsim = 10) 
+Almer_boot_obj$fixef
+Almer_boot_obj$vcov
 
 ## -----------------------------------------------------------------------------
-phylH(mod, numerator = "species", nsim = 3)
+# The number of bootstrap simulations is kept very low in the interest 
+# of computational speed. Often 1000 is used in real analyses.
+phylH_obj <- phylH(mod, numerator = "species", nsim = 10) 
+phylH_obj$phylH
 
